1cb5f3f37b56cd5e0c67d19394413d9fa973ac39
Anton Zarubin <anton.zarubin@gmail.com>
2026-07-19 16:25:05 +0300

    Social preview

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.catemup.battdeck"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.4.0"
+        versionCode = 5
+        versionName = "0.5.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildFeatures { compose = true; buildConfig = true }
